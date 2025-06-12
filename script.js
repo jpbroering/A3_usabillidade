d276ce2216ab32c1be60ba55835d6e9a42b4415b
jpbroering <102531849+jpbroering@users.noreply.github.com>
2025-06-12 07:48:47 -0300

    Add: passando projeto para o github

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+function verificaCEP(cep) {
+    if (cep.length == 9 && cep.indexOf("-") == 5) {
+            cep = cep.replace(/[^0-9]/g, "");
+            getCEP(cep);
+    } else {
+        alert("Formato de CEP inválido. Formato: 00000-000");
+    }
+
+}
+
+async function getCEP(cep) {
+    try {
+        var resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        if (!resposta.ok) throw new Error('Erro ao buscar o CEP');
+        var dados = await resposta.json();
+        mostraCEP(dados);
+    } catch (erro) {
+        alert('Não encontrado', erro.message);
+    }
+}
+
+function buscaCEP() {
+    toggleButton();
+    const cep = document.getElementById("inputCEP").value;
+    clear();
+    verificaCEP(cep);
+}
+
+function mostraCEP(dados) {
+    if(dados.cep === undefined){
+        return alert('Não encontrado');
+    }
+    document.getElementById("resultados").innerText = `Resultados de: ${dados.cep}`;
+    
+    document.getElementById("infoContainer").style = "display: block";
+    
+    document.getElementById("localidade").innerText = `${dados.localidade}`;
+    if(dados.logradouro !== "" && dados.complemento !== "" && dados.bairro !== ""){
+        document.getElementById("logradouro").innerText = `${dados.logradouro} ${dados.complemento} - ${dados.bairro}`;
+    } else {
+        document.getElementById("logradouro").innerText = "—";
+    }
+    
+    document.getElementById("uf").innerText = `${dados.estado} - ${dados.uf}`;
+}
+
+function toggleButton(){
+    const btn = document.getElementById("searchButton");
+    btn.classList.remove("fade");
+    void btn.offsetWidth;
+    return btn.classList.add("fade");
+}
+
+function clear() {
+    document.getElementById("infoContainer").style = "display: none";
+    document.getElementById("resultados").innerText = ``;
+}
